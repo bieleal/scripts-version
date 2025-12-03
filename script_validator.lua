d0c9fdd8d6a9e52a37ed2b3471d4f30179221bc4
TS BIEL <157875041+bieleal@users.noreply.github.com>
2025-12-03 03:31:54 -0300

    Update script_validator.lua

--- script_validator.lua
+++ script_validator.lua
@@ -1,6 +1,6 @@
 local HttpService = game:GetService("HttpService")
 
-local url = "https://raw.githubusercontent.com/bieleal/scripts-version/refs/heads/main/scripts_data"
+local url = "https://raw.githubusercontent.com/bieleal/scripts-version/refs/heads/main/scripts_data.lua"
 
 return function(mainKey, subKey)
 	local response = game:HttpGet(url)
